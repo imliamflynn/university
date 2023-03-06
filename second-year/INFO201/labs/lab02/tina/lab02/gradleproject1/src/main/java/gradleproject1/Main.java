78183a9be5db26f676c3b3b5d73bfed26b177f65
Liam Lennon-Flynn <82933053+imliamflynn@users.noreply.github.com>
2023-03-07 12:26:56 +1300

    Adding actual last files

---
+++ second-year/INFO201/labs/lab02/tina/lab02/gradleproject1/src/main/java/gradleproject1/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject1;
+
+/**
+ *
+ * @author liamflynn
+ */
+public class Main {
+
+    public static void printMessage() {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(getMessage());    }
+
+    public static String getMessage() {
+        return "Hello World";
+     }
+    
+}
